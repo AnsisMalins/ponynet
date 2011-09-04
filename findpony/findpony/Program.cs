b729f40214d04e2c23568813553703c27368c3ac
ansis.malins@gmail.com <ansis.malins@gmail.com@de960614-75ac-6eb5-9e88-07cb973112d4>
2011-09-04 06:39:42 +0000

    Imported all my code.

---
+++ findpony/findpony/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace findpony
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			if (Debugger.IsAttached) args = new[] { "255", "0", "0", "15", @"C:\Users\Ansis\Downloads\RGB_24bits_palette_color_test_chart.png" };
+
+			Vector targetColor;
+			float threshold;
+			VectorMap map;
+
+			try
+			{
+				targetColor = new Vector(byte.Parse(args[0]) / 255f, byte.Parse(args[1]) / 255f, byte.Parse(args[2]) / 255f);
+				threshold = byte.Parse(args[3]);
+				using (var bitmap = new Bitmap(args[4]))
+				{
+					map = new VectorMap(bitmap);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return 0;
+			}
+
+			map.ForEach(delegate(Vector i)
+			{
+				float m = Math.Max(Math.Max(i.X, i.Y), i.Z);
+				return m > 0 ? new Vector(i.X / m, i.Y / m, i.Z / m) : new Vector(1, 1, 1);
+			});
+
+			var mark = new Vector(-1, 0, 0);
+			int max = 0;
+			for (int y = 0; y < map.Height; y++)
+			{
+				for (int x = 0; x < map.Width; x++)
+				{
+					if (map[x, y] == mark) continue;
+					int count = FloodFill(map, new Point(x, y), new Sphere(targetColor, threshold), mark);
+					if (count > max) max = count;
+				}
+			}
+
+			if (Debugger.IsAttached)
+			{
+				Console.WriteLine(max.ToString());
+				Console.ReadKey(true);
+			}
+
+			return max;
+		}
+
+		static int FloodFill(VectorMap map, Point origin, Sphere target, Vector fillColor)
+		{
+			var queue = new Queue<Point>(map.Width + map.Height);
+			queue.Enqueue(origin);
+			int count = 0;
+			while (queue.Count > 0)
+			{
+				Point p = queue.Dequeue();
+				Vector v = map[p];
+				if (v == fillColor || !target.Contains(v)) continue;
+				map[p] = fillColor;
+				count++;
+				if (p.X > 0) queue.Enqueue(new Point(p.X - 1, p.Y));
+				if (p.Y > 0) queue.Enqueue(new Point(p.X, p.Y - 1));
+				if (p.X < map.Width - 1) queue.Enqueue(new Point(p.X + 1, p.Y));
+				if (p.Y < map.Height - 1) queue.Enqueue(new Point(p.X, p.Y + 1));
+			}
+			return count;
+		}
+	}
+}
---
+++ findpony/findpony/VectorMap.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace findpony
+{
+	public class VectorMap
+	{
+		public int Height { get { return Size.Height; } }
+		public Size Size { get; private set; }
+		public int Width { get { return Size.Width; } }
+		Vector[] pixels;
+
+		public unsafe VectorMap(Bitmap bitmap)
+			: this(bitmap.Size)
+		{
+			BitmapData bitmapData = bitmap.LockBits(
+				new Rectangle(Point.Empty, Size),
+				ImageLockMode.ReadOnly,
+				PixelFormat.Format32bppArgb);
+			System.Diagnostics.Debug.Assert(bitmapData.Width * 4 == bitmapData.Stride);
+			var ptr = (byte*)bitmapData.Scan0;
+			for (int i = 0; i < pixels.Length; i++)
+			{
+				float a = ptr[3] / 255f;
+				pixels[i] = new Vector(
+					ptr[2] * a / 255, 
+					ptr[1] * a / 255, 
+					ptr[0] * a / 255);
+				ptr += 4;
+			}
+			bitmap.UnlockBits(bitmapData);
+		}
+
+		public VectorMap(Size size)
+		{
+			Size = size;
+			pixels = new Vector[Width * Height];
+		}
+
+		public VectorMap(VectorMap vectorMap)
+			: this(vectorMap.Size)
+		{
+			Buffer.BlockCopy(vectorMap.pixels, 0, pixels, 0, pixels.Length);
+		}
+
+		public Vector this[Point p]
+		{
+			get
+			{
+#if DEBUG
+				if (p.X < 0 || p.X >= Width || p.Y < 0 || p.Y >= Height) throw new ArgumentOutOfRangeException("p");
+#endif
+				return pixels[p.X + p.Y * Width];
+			}
+			set
+			{
+#if DEBUG
+				if (p.X < 0 || p.X >= Width || p.Y < 0 || p.Y >= Height) throw new ArgumentOutOfRangeException("p");
+#endif
+				pixels[p.X + p.Y * Width] = value;
+			}
+		}
+
+		public void ForEach(Converter<Vector, Vector> converter)
+		{
+			for (int i = 0; i < pixels.Length; i++) pixels[i] = converter(pixels[i]);
+		}
+
+		public Vector this[int x, int y]
+		{
+			get
+			{
+#if DEBUG
+				if (x < 0 || x >= Width) throw new ArgumentOutOfRangeException("x");
+				if (y < 0 || y >= Height) throw new ArgumentOutOfRangeException("y");
+#endif
+				return pixels[x + y * Width];
+			}
+			set
+			{
+#if DEBUG
+				if (x < 0 || x >= Width) throw new ArgumentOutOfRangeException("x");
+				if (y < 0 || y >= Height) throw new ArgumentOutOfRangeException("y");
+#endif
+				pixels[x + y * Width] = value;
+			}
+		}
+
+		public unsafe Bitmap ToBitmap()
+		{
+			var bitmap = new Bitmap(Width, Height);
+			BitmapData bitmapData = bitmap.LockBits(
+				new Rectangle(Point.Empty, Size),
+				ImageLockMode.WriteOnly,
+				PixelFormat.Format32bppArgb);
+			System.Diagnostics.Debug.Assert(bitmapData.Width * 4 == bitmapData.Stride);
+			var ptr = (byte*)bitmapData.Scan0;
+			for (int i = 0; i < pixels.Length; i++)
+			{
+				Vector j = pixels[i];
+				ptr[3] = 255;
+				ptr[2] = (byte)(j.X * 255 + 0.5f);
+				ptr[1] = (byte)(j.Y * 255 + 0.5f);
+				ptr[0] = (byte)(j.Z * 255 + 0.5f);
+				ptr += 4;
+			}
+			bitmap.UnlockBits(bitmapData);
+			return bitmap;
+		}
+	}
+}
